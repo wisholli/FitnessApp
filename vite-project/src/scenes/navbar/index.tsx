60c789200ecf0b3443b271da663312cea83adba9
wisholli <osribna4@gmail.com>
2023-02-15 14:22:33 +0200

    create app and added navbar menu

---
+++ vite-project/src/scenes/navbar/index.tsx
@@ -0,0 +1,93 @@
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { useState } from "react";
+import ActionButton from "@/shared/ActionButton ";
+
+type Props = {
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+  isTopOfPage: boolean;
+};
+
+type NavItems = Pick<Props, "selectedPage" | "setSelectedPage"> & {
+  pages: string[];
+};
+
+const NavBarItems = ({ pages, selectedPage, setSelectedPage }: NavItems) => {
+  const linkList = pages.map((page) => {
+    return (
+      <Link
+        page={page}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    );
+  });
+  return <>{linkList}</>;
+};
+
+const Navbar = ({ selectedPage, setSelectedPage, isTopOfPage }: Props) => {
+  const flexBetween = "flex items-center justify-between";
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const navbarBackgroung = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
+
+  return (
+    <nav>
+      <div
+        className={`${navbarBackgroung} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            <img alt="logo" src={Logo} />
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm `}>
+                  <NavBarItems
+                    pages={["Home", "Benefits", "Our Classes", "Contact Us"]}
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sing In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
+                </div>
+              </div>
+            ) : (
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <NavBarItems
+              pages={["Home", "Benefits", "Our Classes", "Contact Us"]}
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
