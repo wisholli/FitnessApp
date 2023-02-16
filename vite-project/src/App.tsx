a7ad038671d48e12af6bc0541fac9d6b23755b6a
wisholli <osribna4@gmail.com>
2023-02-16 15:32:39 +0200

    added footer

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -5,6 +5,7 @@ import { SelectedPage } from "./shared/types";
 import Benefits from "./scenes/benefits";
 import OurClasses from "./scenes/ourClasses/OurClasses";
 import ContactUs from "./scenes/contactUs";
+import Footer from "./scenes/footer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -37,6 +38,7 @@ function App() {
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
       <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
